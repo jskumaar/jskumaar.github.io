7880fe7066dee718e520a19f612b50872c0c3e29
jskumaar <jskumaar@umich.edu>
2025-02-19 17:16:29 +0000

    Deploying to gh-pages from @ jskumaar/jskumaar.github.io@21bf6e5a78a25e556179dca94cd8bdb706fcaa4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,11 +68,6 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
