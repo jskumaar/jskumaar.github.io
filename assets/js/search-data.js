8f9611136e8c8ddd4079f6a1eb5b28e72d1f88ae
jskumaar <jskumaar@users.noreply.github.com>
2025-05-02 18:45:10 +0000

    Deploying to gh-pages from @ jskumaar/jskumaar.github.io@6b1ae3c555f9ad91544be893e76bc53ca697d8e1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,14 +18,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "Some of my exciting projects. More recent work will be updated soon!  My research statement can be found here.",
+          description: "Some of my exciting projects. My research statement can be found here.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Last updated April 2025.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -37,37 +37,48 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+        },{id: "news-co-organized-the-aaai-fall-symposium-on-agent-teaming-in-mixed-motive-situations",
+          title: 'Co-organized the AAAI Fall Symposium on Agent Teaming in Mixed-Motive Situations.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-presented-at-the-hri-2024-workshop-on-explainability-in-human-robot-collaboration-sharing-work-on-modeling-human-learning-from-robot-demonstrations",
+          title: 'Presented at the HRI 2024 Workshop on Explainability in Human-Robot Collaboration, sharing work...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-gave-an-invited-talk-at-the-microsoft-leaders-in-robotics-and-ai-seminar-university-of-maryland-on-machine-teaching-for-transparent-decision-making-in-human-robot-teams",
+          title: 'Gave an invited talk at the Microsoft Leaders in Robotics and AI Seminar,...',
           description: "",
-          section: "News",},{id: "projects-pedestrian-modeling",
-          title: 'Pedestrian Modeling',
-          description: "Developing hybrid-automaton models of long-term urban pedestrian behavior",
+          section: "News",},{id: "news-presented-at-iros-2024-the-work-on-explainable-robot-decision-making-in-groups-titled-understanding-robot-minds-leveraging-machine-teaching-for-transparent-human-robot-collaboration-across-diverse-groups",
+          title: 'Presented at IROS 2024 the work on explainable robot decision-making in groups, titled...',
+          description: "",
+          section: "News",},{id: "projects-explainable-ai-decision-making-in-human-ai-groups",
+          title: 'Explainable AI Decision-Making in Human-AI Groups',
+          description: "A closed-loop machine teaching framework that uses explainable robot demonstrations and particle filters to model and adapt to individual and group beliefs, improving human understanding of robot decision-making in teams.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-pedestrian-modeling",
-          title: 'Pedestrian Modeling',
-          description: "Developing hybrid-automaton models of long-term urban pedestrian behavior",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-pedestrian-trust-in-automated-vehicles-in-virtual-reality",
-          title: 'Pedestrian trust in automated vehicles in virtual reality',
-          description: "",
+            },},{id: "projects-automatic-detection-of-unexpected-ai-behavior-from-human-cues",
+          title: 'Automatic Detection of Unexpected AI Behavior from Human Cues',
+          description: "Evaluating the detection of unexpected AI behavior in autonomous vehicles by analyzing subtle human emotional cues",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-pedestrian-trust-in-real-world-automated-vehicles",
-          title: 'Pedestrian trust in real-world automated vehicles',
-          description: "",
+            },},{id: "projects-pedestrian-behavior-modeling",
+          title: 'Pedestrian Behavior Modeling',
+          description: "Developing explainable models of long-term urban pedestrian behavior",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
+            },},{id: "projects-trustworthy-interaction-between-automated-vehicles-and-pedestrians",
+          title: 'Trustworthy Interaction Between Automated Vehicles and Pedestrians',
+          description: "Understanding and improving pedestrian trust in automated vehicles through behavioral studies and predictive models for safe, interpretable AV-pedestrian interactions.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-trustworthy-interaction-between-autonomous-vehicles-and-drivers",
+          title: 'Trustworthy interaction between autonomous vehicles and drivers',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-team-cooperation-dynamics-in-mixed-motive-teams",
+          title: 'Team cooperation dynamics in mixed-motive teams',
+          description: "Studying factors that shape cooperation in mixed-motive human-AI teams through interactive online games.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -88,13 +99,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://gitlab.com/jskumaar", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
         id: 'social-linkedin',
@@ -118,25 +122,11 @@ ninja.data = [{
           window.open("https://publons.com/a/AAT-7761-2021/", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=8H5KVN8AAAAJ&hl", "_blank");
-        },
-      },{
-        id: 'social-semanticscholar',
-        title: 'Semantic Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.semanticscholar.org/author/40052328", "_blank");
         },
       },{
       id: 'light-theme',
